ca80931d11b849a31f1fd3039111682277d1b4da
ACAM023 <sonalpha023@gmail.com>
2020-08-24 21:26:48 +0300

    Solution

---
+++ challenges/10-Problem/mike-puzzle-3.py
@@ -0,0 +1,20 @@
+def mike_puzzle(contacts, first, second):
+    people = []
+    i = 0
+    while i < contacts:
+        row = [*map(int, input().split())]
+        people.append(row)
+        
+        i += 1
+    
+    score = 0
+    index = 0
+    
+    return 
+    
+
+contacts = int(input().strip())
+
+first, second = map(int, input().split())
+
+print(mike_puzzle(contacts, first, second))
