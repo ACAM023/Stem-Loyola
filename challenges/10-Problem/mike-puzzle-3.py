a35f3fe0bb7b3a8e714231a1c596b9c81ac2146f
ACAM023 <sonalpha023@gmail.com>
2020-08-26 21:24:32 +0300

    Solution passes first test cases

--- challenges/10-Problem/mike-puzzle-3.py
+++ challenges/10-Problem/mike-puzzle-3.py
@@ -8,10 +8,45 @@ def mike_puzzle(contacts, first, second):
         i += 1
     
     score = 0
-    index = 0
+    row = 0
+    col = 0
+        
+    if people[first-1][second-1] == people[second-1][first-1]:
+        if people[first-1][second-1] < 14:
+            return "YES"
+        else:
+            return "NO"
     
-    return score
+    while col < contacts or row < contacts:
+        if people[row][col] == 0:
+            col += 1
+            continue
+        elif people[row][col] in people[first-1] and people[row][col] == people[col][row]:
+            if people[col][second-1] == people[second-1][col]:
+                if people[col][second-1] != 0:
+                    score += people[col][second-1] + people[col][row]
+                
+                    if score < 14:
+                        return "YES"
+                    else:
+                        score = 0
+                        col += 1
+                        continue
+                else:
+                    for member in people[second-1]:
+                        if member == 0:
+                            continue
+                        position = people[second-1].index(member)
+                        if people[position][col] != 0 and people[col][position] == people[position][col]:
+                            score += people[position][col] + people[col][row] + member
+                            
+                            if score < 14:
+                                return "YES"
+                        else:
+                            continue
     
+    return "NO"
+
 
 contacts = int(input().strip())
 
